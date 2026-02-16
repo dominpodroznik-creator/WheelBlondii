6d95d2afe26feb6555bc85fe6abd6d5edf64c40f
dominpodroznik <dominpodroznik@gmail.com>
2026-02-16 11:12:12 +0100

    fix app order

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const cors = require('cors');
-app.use(cors());require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 if (process.env.MONGO_URI) {
